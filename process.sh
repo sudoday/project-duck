865d1986112fba742db074890d357daee3f20117
Rosho123 <rsf.sho@gmail.com>
2017-06-05 21:13:29 -0700

    added code to get ext ip

--- process.sh
+++ process.sh
@@ -1,10 +1,12 @@
 ip="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | sed 's/\.//g')"
+extip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 while true
 do
     date="$(date | sed 's/\:/\,/g' | sed 's/ /\g/g')"
     curl -X PATCH -d '{
 
     "ip": "'$ip'",
+    "extip": "'$extip'",
     "update": "'$date'"
   }' 'https://projectduck-6999c.firebaseio.com/enrolled/'$ip'.json?auth=Q0Pj5lVGrms9DMITLbuAzWmOpUafqJEw0Cg6j0se'
     update="$(curl 'https://projectduck-6999c.firebaseio.com/update/update.json?shallow=true&print=pretty')"
